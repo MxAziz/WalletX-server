76b250ff402cf771e016f5b2058cced6ec86668e
MxAziz <hereabdulaziz@gmail.com>
2025-09-02 15:45:58 +0600

    add app Error file and all helper file

---
+++ src/app/helpers/handleCastError.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+import { TGenericErrorResponse } from "../interfaces/error.types"
+
+export const handleCastError = (err: mongoose.Error.CastError): TGenericErrorResponse => {
+    return {
+        statusCode: 400,
+        message: "Invalid MongoDB ObjectID. Please provide a valid id",
+        errorSources: [
+            {
+                path: err.path,
+                message: err.message
+            }
+        ]
+    }
+}
