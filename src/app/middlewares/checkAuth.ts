62c2441f1b46d160c540a63bf7c8d90f949d2aa1
MxAziz <hereabdulaziz@gmail.com>
2025-09-06 16:10:49 +0600

    add check auth middleware

---
+++ src/app/middlewares/checkAuth.ts
@@ -0,0 +1,55 @@
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { JwtPayload } from "jsonwebtoken";
+import { envVars } from "../config/env";
+import { Role } from "../modules/user/user.interface";
+import { Wallet } from "../modules/wallet/wallet.model";
+import { verifyToken } from "../utils/jwt";
+import AppError from "../errorHelpers/AppError";
+
+export const checkAuth =
+  (...authRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const accessToken = req.cookies.accessToken || req.headers.authorization;
+
+        if (!accessToken)
+            throw new AppError(StatusCodes.FORBIDDEN, "No token received");
+
+        const verifiedToken = verifyToken(
+            accessToken,
+            envVars.JWT_ACCESS_TOKEN_SECRET
+        ) as JwtPayload;
+
+        if (!authRoles.includes(verifiedToken.role)) {
+            throw new AppError(403, "You are not authorize to access this route");
+        }
+
+        if (
+            verifiedToken.role === Role.USER ||
+            verifiedToken.role === Role.AGENT
+        ) {
+            const isWalletExist = await Wallet.findOne({
+            owner: verifiedToken.userId,
+            });
+
+            if (!isWalletExist)
+            throw new AppError(StatusCodes.NOT_FOUND, "Wallet doesn't exist");
+
+            if (isWalletExist.isBlocked) {
+            throw new AppError(StatusCodes.BAD_REQUEST, "User Wallet is Blocked");
+            }
+        }
+
+            // req.user = verifiedToken;  -> ata chilo agee  me :)
+        req.user = {
+            ...verifiedToken,
+            fullname: verifiedToken.fullname || "",
+            phone: verifiedToken.phone || "",
+            password: verifiedToken.password || ""
+        };
+        next();
+    } catch (error) {
+      next(error);
+    }
+  };
